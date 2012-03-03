1c90353364ab45b91655b81c39000b49b89e74de
Gavin Anderegg <gavin.anderegg@gmail.com>
2012-03-03 19:33:54 -0400

    Mysql

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'towerAwesome',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'passwr0d',                  # Not used with sqlite3.
