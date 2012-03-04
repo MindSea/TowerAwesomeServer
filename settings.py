1d12734b7a390f5133b9329b9e49df72d32e35ce
Gavin Anderegg <ganderegg@mtlpr.ca>
2012-03-03 20:54:23 -0400

    settings sep

--- settings.py
+++ settings.py
@@ -10,17 +10,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'towerAwesome',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'passwr0d',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -148,3 +137,5 @@ LOGGING = {
 }
 
 APPEND_SLASH=False
+
+from settingsLocal import *
